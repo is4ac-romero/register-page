5b4dc32046c44082951a750c23a8adb9921b6369
is4ac-romero <is4ac.rom3ero@hotmail.com>
2025-02-17 20:43:38 -0300

    verification + session

--- session-login.php
+++ session-login.php
@@ -12,7 +12,33 @@
             header("Location: ./index.php?sign=empty-email");
         } else if(empty($password)) { //the only case that i want to save information is when the password is not filled and the email is
             header("Location: ./index.php?sign=empty-password&email=$email");
-        } //else {
+        } else {
+            try {
+                include_once "./includes/db-connection.php";
+                
+                //catch the hash password from DB
+                $stmt = $db->prepare("SELECT id, pass FROM users WHERE email = :email");
+                $stmt->bindParam(":email", $email);
+                $stmt->execute();
+                $result = $stmt->rowCount();
+                
+                //if it returns row
+                if($result) {
+                    $row = $stmt->fetch(PDO::FETCH_ASSOC); //make an array to use the rows by the columns
+                    $passwordVerify = password_verify($password, $row["pass"]); //verify the password that came from db and the input password
 
-        //}
+                    if($passwordVerify) { 
+                        //start a new session
+                        session_start();
+                        $_SESSION["user-id"] = $row["id"]; 
+                    } else {
+                        echo "try review your data";
+                    }
+                } else {
+                    echo "not ok";
+                }
+            } catch(Exception $e) {
+                echo $e->getMessage();
+            }
+        }
     }
